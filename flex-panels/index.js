9a6bd27c8b657d247e14f75819ba62e3ddd786b5
Aprox <36741842+realaprox@users.noreply.github.com>
2022-06-11 00:26:53 +0300

    better Flex-panel gallery

--- flex-panels/index.js
+++ flex-panels/index.js
@@ -2,13 +2,10 @@
 const panels = document.querySelectorAll(`.panel`)
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
-panels.forEach(panel => panel.addEventListener('transitionend', toggleACtive))
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
 
-function toggleOpen() {
-    this.classList.toggle('open')
-}
-function toggleACtive(e) {
-    if(e.propertyName.includes('flex')){
-        this.classList.toggle('active')
-    }
+function toggleOpen() { this.classList.toggle('open') }
+
+function toggleActive(e) {
+    if(e.propertyName.includes('flex')) this.classList.toggle('active')
 }
