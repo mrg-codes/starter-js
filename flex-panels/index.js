f4843b6d3195a9f9cbcadf143a5f3b926ee49a03
Aprox <36741842+realaprox@users.noreply.github.com>
2022-06-11 01:13:05 +0300

    toggleOpen logic fix

--- flex-panels/index.js
+++ flex-panels/index.js
@@ -4,8 +4,20 @@ const panels = document.querySelectorAll(`.panel`)
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive))
 
-function toggleOpen() { this.classList.toggle('open') }
+function toggleOpen() {
+    // only one .open is allowed
+    panels.forEach(panel => panel.classList.remove('open'))
+    // close open element if clicked again
+    if(!this.classList.contains('active')) this.classList.add('open')
+}
 
 function toggleActive(e) {
-    if(e.propertyName.includes('flex')) this.classList.toggle('active')
+    if(e.propertyName.includes('flex')){
+        // prevent from spam loop to add .active when element is closed
+        if(this.classList.contains('open')){
+            this.classList.add('active')
+            return
+        }
+        this.classList.remove('active')
+    }
 }
