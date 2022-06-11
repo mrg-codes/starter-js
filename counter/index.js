f368e05009aad381628f0e57ecda14462d499404
Aprox <36741842+realaprox@users.noreply.github.com>
2022-06-11 14:55:17 +0300

    smart counter logit improvement & global styles fix

--- counter/index.js
+++ counter/index.js
@@ -16,36 +16,45 @@ function doCount(){
 }
 
 function setCounter() {
-    if(!storedCount) return updateCounter('init')
-    updateCounter('show')
+    if(storedCount) return updateCounter('load')
+    updateCounter('init')
 }
 
 function updateCounter(type) {
+    let count = counter.textContent || '0'
+    // determine action
     switch(type){
         case 'dec' :
             count--
-            localStorage.setItem(dataName, count)
             break
         case 'inc' :
             count++
-            localStorage.setItem(dataName, count)
             break
         case 'reset' :
             count = 0
-            localStorage.setItem(dataName, count)
             break
         case 'init' :
             count = 0
+            break
+        case 'save' :
+            count = counter.textContent
             localStorage.setItem(dataName, count)
             break
-        case 'show' :
-            count = storedCount
-            localStorage.setItem(dataName, count)
+        case 'load' :
+            count = localStorage.getItem(dataName)
+            console.log(count)
             break
         case 'clear' :
             count = 0
             localStorage.removeItem(dataName)
             console.log(`${dataName} cleared`)
     }
+    // counter value colors
+    let newColor = 'lighttext'
+    if(count > 0) newColor = 'success'
+    if(count < 0) newColor = 'danger'
+    document.documentElement.style.setProperty(`--counter-color`, `var(--${newColor})`)
+    console.log(`${count} and --${newColor}`)
+    // update counter element
     counter.textContent = count
 }
