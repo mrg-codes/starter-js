c4616861c19e02365c404f020586ef133ca1c49d
Aprox <36741842+realaprox@users.noreply.github.com>
2022-06-12 00:40:09 +0300

    lib-list auto generate from structure mvp

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 // vars
 const url = '/projects/structure.json'
-let list = []
+let structureData = []
+let projectList = []
+const libList = document.querySelector('.lib-list')
 
+window.onload = generateList
 // grab projectr data from json
 fetch(url)
     .then(res => res.json())
-    .then(data => list.push(...data.Projects))
+    .then(data => {
+        structureData.push(data)
+        projectList.push(...data.Projects) 
+    })
     .catch((error) => {
         console.error(error)
     })
-console.log(list)
+
+function generateList(){
+    if(!projectList) return console.log(`no list`)
+    const projectPanelHtml = projectList.map(project => {
+        const id = project.id
+        const name = project.name
+        const ref = project.reference
+        const desc = project.description
+        const main = project.main
+        const logic = project.logic
+        return `
+        <li>
+            <h3>${id} ${name}</h3>
+            <p>${desc}</p>
+            <a href=".${ref}${main}" class="bg-low-darken hover-primary trans100"><i class="fas fa-tv"></i>Live Preview</a>
+        </li>
+        `
+    }).join('')
+    console.log(libList)
+    libList.innerHTML = projectPanelHtml
+    console.log(projectPanelHtml)
+}
