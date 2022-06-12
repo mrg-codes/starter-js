420a454a01fcae1c90cb92b8349a7856303f7cfe
Aprox <36741842+realaprox@users.noreply.github.com>
2022-06-12 15:58:38 +0300

    structure.json reworkd & lib implement

--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 const url = '/projects/structure.json'
 let structureData = []
 let projectList = []
+let socialList = []
 const libList = document.querySelector('.lib-list')
+const footerSocials = document.querySelector('.foot-socials')
 
-window.onload = setTimeout(generateList, 500)
 // grab projectr data from json
 fetch(url)
-    .then(res => res.json())
-    .then(data => {
+    .then(res => res.json()).then(data => {
         structureData.push(data)
         projectList.push(...data.Projects) 
-    })
-    .catch((error) => {
+        socialList.push(...data.Socials) 
+    }).catch((error) => {
         console.error(error)
-    })
+})
 
-function generateList(){
+function generateLists(){
+    // check fi project lsit is not empty
     if(!projectList) return console.log(`no list`)
+
+    // generate libraries
     const projectPanelHtml = projectList.map(project => {
         const id = project.id
         const name = project.name
@@ -32,11 +35,25 @@ function generateList(){
             <span class="name">${name}</span>
             </h3>
             <p>${desc}</p>
-            <a href=".${ref}${main}" class="preview"><i class="fas fa-tv"></i>${linkText}</a>
+            <a href=".${ref}${main}" target="_blank" class="preview"><i class="fas fa-tv"></i>${linkText}</a>
         </li>
         `
     }).join('')
-    console.log(libList)
+
+    // generate footer stuff
+    const footerSocialsHtml = socialList.map(social => {
+        const name = social.name
+        const link = social.link
+        const customlink = social.customlink
+        const username = social.username
+        return `
+        <a href="${link}${customlink}" target="_blank" class="foot-social foot-${name}">
+            <i class="fa-brands fa-${name}"></i></a>
+        `
+    }).join('')
+    footerSocials.innerHTML = footerSocialsHtml
     libList.innerHTML = projectPanelHtml
-    console.log(projectPanelHtml)
 }
+
+// 👉 LAST THING TO DO!!! 👈 ❗❗❗
+window.onload = generateLists
